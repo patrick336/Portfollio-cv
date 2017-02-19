7eb17c13887297f8ca316e96461d48b5a66140c8
patrick336 <p.kalinowski.93@gmail.com>
2017-02-19 15:28:00 +0100

    update

--- js/App.js
+++ js/App.js
@@ -1,4 +1,30 @@
- $(window).on('click','carousel', function(event) {
-    	alert("ojej");
-     
+$(document).ready(function () {
+
+//	 Add scrollspy to <body>
+	$('body').scrollspy({
+		target: ".navbar",
+		offset: 50
+	});
+	// Add smooth scrolling on all links inside the navbar
+	$("#myNavbar a,.navbar-header a").on('click', function (event) {
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
+			// Store hash
+			var hash = this.hash;
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			
+			var distance = $('.navbar').height();		
+				   
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top-distance
+			}, 800, function () {
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			});
+		} // End if
+	});
 });
+
